658e8e0e48f426d3cbf54cae7aa85c19e099d43e
Aaron Ofengender <aofengen@gmail.com>
2017-10-11 10:11:30 -0400

    created create ticket page

--- src/app/router.module.ts
+++ src/app/router.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HistoryComponent } from './history/history.component';
-
+import { SHomeComponent } from './shome/shome.component';
 import { CreateTicketComponent } from './create-ticket/create-ticket.component';
 
 export const routes = [
     { path: '', redirectTo: '/landing', pathMatch: 'full' },
     { path: 'landing', component: LandingPageComponent},
     { path: 'history', component: HistoryComponent },
-
+    { path: 'home', component: SHomeComponent },
     { path: 'create', component: CreateTicketComponent}
  ];
