bcb710371d6e3966969743417bc94a37d3812a49
Kaylea Britton <kbbritton217@gmail.com>
2017-10-11 12:03:48 -0400

    Added Admin Home

--- src/app/router.module.ts
+++ src/app/router.module.ts
@@ -6,12 +6,14 @@ import { HistoryComponent } from './history/history.component';
 import { SHomeComponent } from './shome/shome.component';
 import { CreateTicketComponent } from './create-ticket/create-ticket.component';
 import { SdetailComponent } from './sdetail/sdetail.component';
+import { AhomeComponent } from './ahome/ahome.component';
 
 export const routes = [
     { path: '', redirectTo: '/landing', pathMatch: 'full' },
     { path: 'landing', component: LandingPageComponent},
     { path: 'history', component: HistoryComponent },
     { path: 'home', component: SHomeComponent },
+    { path: 'admin/home', component: AhomeComponent },
     { path: 'create', component: CreateTicketComponent },
     { path: 'update', component: SdetailComponent }
  ];
