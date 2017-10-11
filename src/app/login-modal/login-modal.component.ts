3868bc667e9db743b8f9e636414c27aa7a14b526
Aaron Ofengender <aofengen@gmail.com>
2017-10-11 14:08:35 -0400

    added ability to login

--- src/app/login-modal/login-modal.component.ts
+++ src/app/login-modal/login-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login-modal',
@@ -8,9 +10,25 @@ import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class LoginModalComponent implements OnInit {
 
-  constructor(public activeModal: NgbActiveModal) {}
+	token: string;
+
+  constructor(public activeModal: NgbActiveModal, private router: Router) {}
 
   ngOnInit() {
   }
+
+  onLogin(email: string, password: string) {
+  	firebase.auth().signInWithEmailAndPassword(email, password)
+  	.then(
+  		response => {	
+  			firebase.auth().currentUser.getIdToken()
+  			.then(
+  				(token: string) => this.token = token
+  			)
+  		})
+  	.catch(error => console.log(error))
+	  this.activeModal.close('Close click');
+	  this.router.navigate(['/home']);
+	}
 
 }
