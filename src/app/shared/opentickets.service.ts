f6cc1a682289ed222d10c828fdd69a7bb18c7909
Devon Brosch <devon@brosch.org>
2017-10-16 10:15:41 -0400

    get openIssues once, added

--- src/app/shared/opentickets.service.ts
+++ src/app/shared/opentickets.service.ts
@@ -4,32 +4,22 @@ import { EventEmitter } from '@angular/core';
 
 export class OpenTicketService {
 	openTicketsChanged = new EventEmitter<OpenTickets[]>();
-	private tickets: OpenTickets[] =[
-	new OpenTickets('Devon Brosch', 'test', false, 'angular 4', 'Kenn', 'Launch', '5:00pm'),
-	new OpenTickets('Aaron Ofengender', 'test2', false, 'angular 5', 'Paul', 'Cali', '8:00am')
-	];
+	private tickets: OpenTickets[] = [];
 
-	constructor() { }
+	constructor() {
+	 }
 
 	getOpenTickets() {
+		let tickets = []
 		let database = firebase.database();
 		let openIssuesRef = database.ref('openIssues').orderByKey();
 		openIssuesRef.once('value').then(function(snapshot) {
 	    	snapshot.forEach(function(childSnapshot) {
 	    	let key = childSnapshot.key;
 	      	let childData = childSnapshot.val();
-	      	let openTickets = {
-				name: childData.name,
-				description: childData.desc,
-				issueSolved: childData.issueSolved,
-				category: childData.category,
-				instructor: childData.instructor,
-				location: childData.location,
-				time: childData.time
-			}
-			this.tickets.push(openTickets)
+			tickets.push(childData);
 	    	});
 		});	
-		return this.tickets	
+		return this.tickets = tickets;
 	}
 }
