068bc992161e3f4fdfec16dc906b2d1f9fb2e968
Devon Brosch <devon@brosch.org>
2017-10-24 14:30:27 -0400

    added dropdowns and admin functions to admin settings

--- src/app/admin-settings/admin-settings.component.ts
+++ src/app/admin-settings/admin-settings.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase'; 
 import { AuthService } from '../services/auth.service';
+import { CreateTicketService } from '../services/create-ticket.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-settings',
@@ -9,23 +11,46 @@ import { AuthService } from '../services/auth.service';
 })
 export class AdminSettingsComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  private categories = [];
+  private usernames = [];
+
+  constructor(
+    private authService: AuthService, 
+    private createTicketService: CreateTicketService, 
+    private router: Router) { }
 
   ngOnInit() {
+    this.getCategories();
+    this.getUserNames();
   }
 
-  makeAdmin(username) {
-    this.authService.promoteAdmin(username);
+  makeAdmin(user) {
+    this.authService.promoteAdmin(user);
+    this.router.navigate(['admin/ticket']);
   }
 
   addCategory(category) {
     let issues = firebase.database().ref("issueCategory")
-    issues.update({category: category});
+    let categoryKey = category;
+    let categoryData = categoryKey;
+    let categoryObject = {};
+    eval("categoryObject." + categoryKey + " = '" + categoryData + "'");
+    issues.update(categoryObject)
+    this.router.navigate(['admin/ticket']);
   }
 
   deleteCategory(category) {
     alert("This will delete an item from the Issue Category list");
-    // let issues = firebase.database().ref("issueCategory/")
-    // issues.remove(category);
+    let issues = firebase.database().ref("issueCategory/" + category)
+    issues.remove()
+    this.router.navigate(['admin/ticket']);
+  }
+  getCategories() {
+    this.categories = this.createTicketService.getCategories();
+    return this.categories
+  }
+
+  getUserNames() {
+    this.usernames = this.authService.getAllUser()
   }
 }
