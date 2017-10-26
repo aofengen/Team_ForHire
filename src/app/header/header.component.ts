d0f453f5f3196a0218d9ac24345961518cd095d0
Aaron Ofengender <aofengen@gmail.com>
2017-10-26 11:36:58 -0400

    edited header to include name of app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,8 +10,6 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 userInfo: Users[];
-
-title = 'HELP!!!'
   
   constructor(private authService: AuthService) { }
 
